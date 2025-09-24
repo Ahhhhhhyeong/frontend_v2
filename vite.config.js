f805878df356b6c0b23c632fb82d3ffc740c2589
Ahhhhhyeong <dkdud081@gmail.com>
2025-09-24 17:10:58 +0900

    [refactor] backup 파일 정리 및 리팩토링을 위한 라이브러리 설치

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr'; // 1. svgr을 import 합니다.
+import path from 'path';
 
 export default defineConfig({
-   // 2. plugins 배열에 svgr()을 추가합니다.
-   plugins: [react(), svgr()],
+  // 2. plugins 배열에 svgr()을 추가합니다.
+  plugins: [react(), svgr()],
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.js',
+  },
 });
