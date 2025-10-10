a266330083a8b3431d1e0c3c5650a3e2ee43a09e
Ahhhhhyeong <dkdud081@gmail.com>
2025-10-10 16:10:27 +0900

    [refactor] 1차 커밋

--- src/components/FormLayout.jsx
+++ src/components/FormLayout.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import styles from './FormLayout.module.css';
+import { Toaster } from 'react-hot-toast';
 
 export default function FormLayout() {
   return (
     <div className={styles.container}>
+      <Toaster position='top-center' reverseOrder={false} />
       <div className={styles.mobileScreen}>
         <Outlet />
       </div>
